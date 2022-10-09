2432941c9fcdaed55c45273fd2d365cb460465de
dariel0514 <dariel0514@hotmail.com>
2022-10-09 17:24:41 +0000

    Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,47 +1,50 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-	const [list, setList] = useState([])
-	
-	useEffect(() => {
-		getList()
-	},[])
+  const [list, setList] = useState([]);
 
-const getList = () => {
-	fetch("http://assets.breatheco.de/apis/fake/todos/user/dariel")
-  .then(response => response.json())
-  .then(result => setList(result))
-  .catch(error => console.log(error))
-	}
+  useEffect(() => {
+    getList();
+  }, []);
 
-const addTask = (myTask) => {
-	const newList = [...list, myTask]
-	fetch("http://assets.breatheco.de/apis/fake/todos/user/dariel",{
-	method: 'PUT',
-	headers: {
-	'Content-Type': 'application/json'
-	},
-	body: JSON.stringify(newList),
-	redirect: 'follow'
-	})
-	.then(response => response.json())
-	.then(result => getList())
-	.catch(error => console.log(error))
-}
+  const getList = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/dariel")
+      .then((response) => response.json())
+      .then((result) => {
+		console.log(result)
+		setList(result)})
+      .catch((error) => console.log(error));
+  };
 
-return (
-	<div className="text-center">
-		{list.map((task, i) => {
-			return(
-				<p key={i}>{task.label}</p>
-			)
-		})}
-	<button
-	className="btn btn-primary"
-	onClick={() => addTask([{label: 'Eat', done: false}])}
-	>Add</button>
-	</div>
-);
+  const addTask = (myTask) => {
+    var newList = [...list, myTask];
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/dariel", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newList),
+      redirect: "follow",
+    })
+      .then((response) => response.json())
+      .then((result) => getList())
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="text-center">
+      {list.map((task, i) => {
+        return <p key={i}>{task.label}</p>;
+      })}
+      <h1>To Do List</h1>
+      <button
+        className="btn btn-success"
+        onClick={() => addTask({ label: "Eat", done: false })}
+      >
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default Home;
